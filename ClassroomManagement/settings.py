4f36bc2bce56d5951ef9fea726349c720b52073f
VyDuong162 <69305591+VyDuong162@users.noreply.github.com>
2022-06-21 14:07:01 +0700

    cập nhật model

--- ClassroomManagement/settings.py
+++ ClassroomManagement/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'classroom',
 ]
 
 MIDDLEWARE = [
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'ClassroomManagement.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'classroommgr',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
--- classroom/models.py
+++ classroom/models.py
@@ -1,3 +1,59 @@
 from django.db import models
 
-# Create your models here.
+class Student(models.Model) :
+    student_id= models.IntegerField(primary_key=True,max_length=10)
+   first_name = models.CharField(
+                max_length=50, help_text="The student's first name")
+   last_name = models.CharField(
+                max_length=50, help_text="The student's last name")
+   email = models.EmailField(help_text="The contact email for the student")
+    roll_no = models.IntegerField(help_text="The roll number of the student")
+    username = models.CharField(unique=True,max_length=100, help_text="Username's student")
+    password = models.CharField(max_length=100, help_text="Password's username")
+
+    def __str__(self):
+        return self.first_name
+
+
+class Sections(models.Model):
+    section_id = models.IntegerField(primary_key=True, max_length=10)
+    title = models.CharField(max_length=100, help_text="The title's resource")
+    create_date = models.DateTimeField(auto_now_add=True, help_text="The date and time the resource was created")
+    room_id = models.ForeignKey(Classroom, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.title
+
+
+class Assignment(models.Model):
+    Assignment_id = models.IntegerField(primary_key=True, max_length=10)
+    file = models.FileField(help_text="The file of assignment", blank=True, upload_to="assignments_files/)
+    create_date = models.DateTimeField(auto_now_add=True, help_text="The date and time the resource was created")
+    deadline_date = models.DateTimeField(null=True, help_text=\
+                     "The date and time the assignment is deadline.")
+    textbox = models.TextField(help_text="The comment of The assignment")
+    section_id = models.ForeignKey(Sections, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100, help_text="The title's assignment")
+
+    def __str__(self):
+        return self.title
+
+
+class Submits(models.Model) :
+    student_id = models.ForeignKey (Student, on_delete=models.CASCADE)
+    assignment_id = models.ForeignKey(Assignment, on_delete=models.CASCADE)
+    submitted_on = models.SmallIntegerField(help_text="The comment of The resource")
+    status = models.SmallIntegerField(help_text="The status of submit has 2 value: 0: yet , 1: done")
+    file = models.FileField(help_text="The file of submit",blank=True, upload_to="Submits_files/")
+
+
+class Resources(models.Model) :
+    resource_id= models.IntegerField(primary_key=True, max_length=10)
+    title = models.CharField(max_length=100, help_text="The title's resource")
+    create_date = models.DateTimeField(auto_now_add=True, help_text="The date and time the resource was created")
+    textbox = models.TextField(help_text="The comment of The resource")
+    file = models.FileField(help_text="The file of resource")
+    section_id = models.ForeignKey(Sections, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
