544db1d6186b805b52f3d2e38ef751533eb93234
HungNguyen1311 <97245599+HungNguyen1311@users.noreply.github.com>
2022-07-01 10:56:56 +0700

    Update models.py

--- classroom/models.py
+++ classroom/models.py
@@ -39,7 +39,7 @@ class Classes(models.Model):
 
 
 class Student(models.Model):
-    student_id = models.IntegerField(primary_key= True)
+    student_id = models.AutoField(primary_key= True)
     class_id = models.ForeignKey(Classes, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50, help_text="The Student's first name or names.")
     last_name = models.CharField(max_length=50, help_text="The Student's last name or names.")
@@ -49,9 +49,11 @@ class Student(models.Model):
 
     def __str__(self):
         return self.first_name
+     class Meta:
+        db_table="classroom_student"
 
 class Teacher(models.Model):
-    teacher_id = models.IntegerField(primary_key=True)
+    teacher_id = models.AutoField(primary_key=True)
     stream_id = models.ForeignKey(Stream, on_delete=models.CASCADE)
     first_name = models.CharField(max_length=50, help_text="The Teacher's first name or names.")
     last_name = models.CharField(max_length=50, help_text="The Teacher's last name or names.")
@@ -60,7 +62,8 @@ class Teacher(models.Model):
 
     def __str__(self):
         return self.first_name
-
+    class Meta:
+        db_table="classroom_teacher"
 
 class Classroom(models.Model):
     room_id = models.IntegerField(primary_key=True)
